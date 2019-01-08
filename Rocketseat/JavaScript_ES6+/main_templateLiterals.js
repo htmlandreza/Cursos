08157f3f3be39c9f4ad174f08b6e47e39a677931
Andreza Moreira <htmlandreza@gmail.com>
2019-01-08 15:43:56 -0200

    Estudo do Template Literals

---
+++ Rocketseat/JavaScript_ES6+/main_templateLiterals.js
@@ -0,0 +1,8 @@
+//variável
+const nome = 'Andreza';
+const idade = 20;
+
+//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.')
+
+//Otimizando com template literals
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
