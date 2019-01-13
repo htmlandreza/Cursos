2aa62047d983336c8c4e5a4cf6d59c7d8e0ee86e
Andreza Moreira <htmlandreza@gmail.com>
2019-01-13 14:47:20 -0200

    Reorganizando pastas

--- Rocketseat/JavaScript_ES6+/exercicio6_mod1.js
+++
@@ -1,5 +0,0 @@
-//Exercício 6
-const usuario = "Diego";
-const idade = 23;
-
-console.log (`O usuário ${usuario} possui ${idade} anos.`)
--- Rocketseat/JavaScript_ES6+/main_templateLiterals.js
+++
@@ -1,8 +0,0 @@
-//variável
-const nome = 'Andreza';
-const idade = 20;
-
-//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.')
-
-//Otimizando com template literals
-console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
